b5a14f7f5beebaf0ffd5adfc048cbcaf8f5d20f2
Othmane <mahfoud.othmane97@gmail.com>
2018-05-26 16:08:09 +0000

    Created a data model, requested and parsed JSON from GitHub API and update view with new data

--- GitStars/Controller/RepoListViewController.swift
+++ GitStars/Controller/RepoListViewController.swift
@@ -7,8 +7,17 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class RepoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //Constants
+    let requestURL : String = "https://api.github.com/search/repositories?q=created:%3E2017-10-22&sort=stars&order=desc"
+    let indexOfLastRepo : Int = 29
+    
+    //Variables
+    var allRepos : [Repo] = [Repo]()
 
     @IBOutlet weak var repoListTableView: UITableView!
     
@@ -25,6 +34,9 @@ class RepoListViewController: UIViewController, UITableViewDelegate, UITableView
         //Adjust table's cells automatically
         configureTableView()
         
+        //Get data from api
+        getRepoData(url: requestURL)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,25 +45,81 @@ class RepoListViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     //MARK: - TableView DataSource Methods
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "repoCustomCell", for: indexPath) as! RepoCustomCell
-        let testDataArray = ["sdfghjklfdscvbghnjkfdcasxfvghbjnkmfsdavbjnkmfvdcsxcfhgvjbnfedcsvhsdfghjklfdscvbghnjkfdcasxfvghbjnkmfsdavbjnkmfvdcsxcfhgvjbnfedcsvhsdfghjklfdscvbghnjkfdcasxfvghbjnkmfsdavbjnkmfvdcsxcfhgvjbnfedcsvh", "sdfghjklfdscvbghnjkfdcasxfvghbjnkmfsdavbjnkmfvdcsxcfhgvjbnfedcsvhsdfghjklfdscvbghnjkfdcasxfvghbjnkmfsdavbjnkmfvdcsxcfhgvjbnfedcsvhsdfghjklfdscvbghnjkfdcasxfvghbjnkmfsdavbjnkmfvdcsxcfhgvjbnfedcsvh", "sdfghjklfdscvbghnjkfdcasxfvghbjnkmfsdavbjnkmfvdcsxcfhgvjbnfedcsvhsdfghjklfdscvbghnjkfdcasxfvghbjnkmfsdavbjnkmfvdcsxcfhgvjbnfedcsvhsdfghjklfdscvbghnjkfdcasxfvghbjnkmfsdavbjnkmfvdcsxcfhgvjbnfedcsvh"]
         
-        cell.repoDescriptionLabel.text = testDataArray[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "repoCustomCell", for: indexPath) as! RepoCustomCell
+        
+        cell.repoNameLabel.text = allRepos[indexPath.row].repoName
+        cell.repoDescriptionLabel.text = allRepos[indexPath.row].repoDescription
+        cell.repoOwnerNameLabel.text = allRepos[indexPath.row].repoOwner
+        if allRepos[indexPath.row].repoStars >= 1000 {
+            
+            let formatedStars = allRepos[indexPath.row].repoStars / 1000
+            cell.repoStarsLabel.text = "\(String(NSString(format: "%.01f", formatedStars)))k"
+            
+        }
+        else{
+            cell.repoStarsLabel.text = String(allRepos[indexPath.row].repoStars)
+        }
+        
         return cell
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 3; //to be replaced with the actual number of rows needed
+
+        return allRepos.count
         
     }
     
     //Configure the Table View cells to have an optimal height
+    
     func configureTableView() {
         
         repoListTableView.rowHeight = UITableViewAutomaticDimension
         repoListTableView.estimatedRowHeight = 120.0
+        
+    }
+    
+    //MARK: - Networking
+    
+    func getRepoData(url: String) {
+        
+        Alamofire.request(url, method: .get).responseJSON { response in
+            if response.result.isSuccess {
+                print("Sucess! Got the github data")
+                let repoJSON : JSON = JSON(response.result.value!)
+                self.updateRepoData(json: repoJSON)
+            }
+            else {
+                print("Error: \(String(describing: response.result.error))")
+            }
+        }
+        
+    }
+    
+    //MARK: - JSON Parsing
+    
+    func updateRepoData(json : JSON) {
+        
+        for index in 0...indexOfLastRepo {
+            if let repoName = json["items"][index]["name"].string {
+                let repoDescription = json["items"][index]["description"].string
+                let repoOwner = json["items"][index]["owner"]["login"].string
+                let repoOwnerAvatar = json["items"][index]["owner"]["avatar_url"].string
+                let repoStars = json["items"][index]["stargazers_count"].float
+                let newRepo = Repo(name: repoName, description: repoDescription!, owner: repoOwner!, ownerAvatar: repoOwnerAvatar!, stars: repoStars!)
+                
+                self.allRepos.append(newRepo)
+                
+                self.configureTableView()
+                self.repoListTableView.reloadData()
+            }
+            else{
+                print("Error: could not parse data")
+            }
+        }
         
     }
 
---
+++ GitStars/Model/Repo.swift
@@ -0,0 +1,27 @@
+//
+//  Repo.swift
+//  GitStars
+//
+//  Created by Othmane on 5/26/18.
+//  Copyright © 2018 Othmane. All rights reserved.
+//
+
+import Foundation
+
+class Repo {
+    
+    var repoName : String
+    var repoDescription : String
+    var repoOwner : String
+    var repoOwnerAvatar : String
+    var repoStars : Float
+    
+    init(name : String, description : String, owner : String, ownerAvatar : String, stars : Float) {
+        repoName = name
+        repoDescription = description
+        repoOwner = owner
+        repoOwnerAvatar = ownerAvatar
+        repoStars = stars
+    }
+    
+}
